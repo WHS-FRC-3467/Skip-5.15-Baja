171eab7654085b00fa5c565ea07bb1e42b36893a
Driver3467 <FRC_3467_Driver@team3467.org>
2025-04-10 21:38:38 -0400

    l4 practice field setpoints

--- src/main/java/frc/robot/subsystems/Arm/Arm.java
+++ src/main/java/frc/robot/subsystems/Arm/Arm.java
@@ -22,6 +22,7 @@ public class Arm extends GenericMotionProfiledSubsystem<Arm.State> {
         LEVEL_2(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(97.48), 0)),
         LEVEL_3(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(104.48), 0)),
         LEVEL_4(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(101.33), 0)),
+        // LEVEL_4(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(98.33), 0)),
         CLIMB(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(73.44), 0)),
         ALGAE_LOW(new ProfileType.MM_POSITION(() -> Units.degreesToRotations(103.3), 0)),
         // ALGAE_LOW_P(new ProfileType.MM_POSITION(() -> .2377, 0)),
