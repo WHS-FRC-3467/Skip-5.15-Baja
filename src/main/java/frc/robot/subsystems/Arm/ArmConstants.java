4af0aac9bfb2570d8fbc0b2115fd06681e516caa
Driver3467 <frc_3467_driver@team3467.org>
2025-10-16 20:12:31 -0400

    Update at 'Thu Oct 16 20:12:31 EDT 2025'

--- src/main/java/frc/robot/subsystems/Arm/ArmConstants.java
+++ src/main/java/frc/robot/subsystems/Arm/ArmConstants.java
@@ -51,8 +51,14 @@ public final class ArmConstants {
         kSubSysConstants.kMotorConfig.Feedback.FeedbackSensorSource =
             FeedbackSensorSourceValue.FusedCANcoder;
         kSubSysConstants.kMotorConfig.Feedback.SensorToMechanismRatio = 1;
-        kSubSysConstants.kMotorConfig.Feedback.RotorToSensorRatio =
-            (5.0 / 1.0) * (4.0 / 1.0) * (48.0 / 22.0) * (70.0 / 22.0);
+        if (Constants.getRobot() == RobotType.BAJA) {
+            kSubSysConstants.kMotorConfig.Feedback.RotorToSensorRatio =
+                (5.0 / 1.0) * (4.0 / 1.0) * (48.0 / 22.0) * (70.0 / 22.0);
+
+        } else {
+            kSubSysConstants.kMotorConfig.Feedback.RotorToSensorRatio =
+                (9.0 / 1.0) * (48.0 / 22.0) * (70.0 / 22.0);
+        }
 
         // Different encoder offsets for each robot
         double kGortCANcoderOffset = -0.185791015625;
