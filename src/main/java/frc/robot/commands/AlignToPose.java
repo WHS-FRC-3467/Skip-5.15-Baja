485a1b1fa602adb5e1e14744ae7d726d35a0c588
spybh66 <brysonh99@gmail.com>
2025-10-21 18:44:09 -0400

    almost working

--- src/main/java/frc/robot/commands/AlignToPose.java
+++ src/main/java/frc/robot/commands/AlignToPose.java
@@ -4,43 +4,28 @@
 
 package frc.robot.commands;
 
-import static edu.wpi.first.units.Units.Degrees;
-import static edu.wpi.first.units.Units.Meters;
+import java.util.function.DoubleSupplier;
 import java.util.function.Supplier;
 import edu.wpi.first.math.geometry.Pose2d;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.subsystems.drive.Drive;
-import frc.robot.util.LoggedTunableNumber;
 import frc.robot.util.LoggedTuneableProfiledPID;
-import lombok.Getter;
 
-public class DriveToPose extends DriveToPoseBase {
+public class AlignToPose extends AlignToPoseBase {
 
     private final static LoggedTuneableProfiledPID linearController =
         new LoggedTuneableProfiledPID("DriveToPose/LinearController", 3.0, 0, 0.1, 3.0, 0.0);
+
     private final static LoggedTuneableProfiledPID angularController =
         new LoggedTuneableProfiledPID("DriveToPose/AngularController", 3.0, 0, 0, 0, 0);
 
-    private final static LoggedTunableNumber maxLinearVel =
-        new LoggedTunableNumber("DriveToPose/MaxLinearVelocity (m s)", 3.0);
-    private final static LoggedTunableNumber maxAngularVel =
-        new LoggedTunableNumber("DriveToPose/MaxAngularVelocity (rad s)", 9.0);
-
-    public DriveToPose(
-        Drive drive,
-        Supplier<Pose2d> targetPose)
+    public AlignToPose(Drive drive, Supplier<Pose2d> targetPose, AlignMode mode,
+        DoubleSupplier joystickInput)
     {
-        super(
-            drive,
+        super(drive,
             targetPose,
+            mode,
+            joystickInput,
             linearController,
-            angularController,
-            maxLinearVel,
-            maxAngularVel);
-
-
+            angularController);
     }
-
-
-
 }
