e0049e6e3b3713a2bffa42498fae907bcaf6dca5
Driver3467 <frc_3467_driver@team3467.org>
2025-10-23 18:26:08 -0400

    Update at 'Thu Oct 23 18:26:08 EDT 2025'

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -46,7 +46,7 @@ public final class Constants {
         public static boolean isComp;
         public static boolean isAlpha;
 
-        public static final String compSerial = "0001";
+        public static final String compSerial = "0323CA19";
         public static final String alphaSerial = "03223885";
         static {
             if (Robot.isReal()) {
