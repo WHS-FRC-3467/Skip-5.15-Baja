24e15af88afb9daf5ab0a18ff341aa6b194253ce
Driver3467 <frc_3467_driver@team3467.org>
2025-10-24 18:04:21 -0400

    Update at 'Fri Oct 24 18:04:21 EDT 2025'

--- src/main/java/frc/robot/subsystems/Vision/Vision.java
+++ src/main/java/frc/robot/subsystems/Vision/Vision.java
@@ -133,7 +133,7 @@ public class Vision extends SubsystemBase {
                         || observation.pose().getX() > aprilTagLayout.getFieldLength()
                         || observation.pose().getY() < 0.0
                         || observation.pose().getY() > aprilTagLayout.getFieldWidth()
-                        || observation.averageTagDistance() > Units.feetToMeters(20);
+                        || observation.averageTagDistance() > Units.feetToMeters(10);
 
                 // Add pose to log
                 robotPoses.add(observation.pose());
